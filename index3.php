f95956902507fa6bf82f37814ce5e18b77ee580f
floxrl <104560626+floxrl@users.noreply.github.com>
2022-06-27 14:27:20 +0200

    Delete index3.php

--- index3.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "./connect.php";
-$login=$_POST["Supp_con"];
-$sql_update = $db->prepare('UPDATE credentials SET verif_con = 0 where login=?');
-$sql_update->execute([$login]);
-header('Location: ./index.php');
-?>
