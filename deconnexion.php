1029643fd666a9e0659d48f0ea3956500c0f67b1
floxrl <104560626+floxrl@users.noreply.github.com>
2022-06-27 14:26:31 +0200

    Delete deconnexion.php

--- deconnexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    require "./connect.php";
-    $login=$_POST["Deco"];
-    ///////////////// DCONNEXION ////////////////////////
-    $sql_check = $db->prepare('UPDATE credentials SET verif_con = 0 where login=?');
-    $sql_check->execute([$login]);
-    header('Location: ./index.php');
-?>
